dca56ce34b34b671c3f86758b1e8a9d8fffdeffd
angelo gabriel vasconcelos baptista <angelogabriel01@live.com>
2025-04-22 20:30:43 -0300

    implementando todas as operações CRUD de posts

--- server/controller/PostController.py
+++ server/controller/PostController.py
@@ -20,3 +20,32 @@ def criar_post(post_data: PostCreate):
     })
 
     return salvo
+
+@router.get("/", response_model=list[PostResponse])
+def listar_posts():
+    return PostService.buscar_todos_os_posts()
+
+@router.get("/{post_id}", response_model=PostResponse)
+def buscar_post(post_id: int):
+    post = PostService.buscar_post_por_id(post_id)
+    if not post:
+        raise HTTPException(status_code=404, detail="Post não encontrado")
+    return post
+
+@router.put("/{post_id}", response_model=PostResponse)
+def atualizar_post(post_id: int, dados: PostCreate):
+    post_existente = PostService.buscar_post_por_id(post_id)
+    if not post_existente:
+        raise HTTPException(status_code=404, detail="Post não encontrado")
+
+    post_existente.conteudo = dados.conteudo
+    post_existente.autor_id = dados.autor_id
+
+    return PostService.criar_post(post_existente)
+
+@router.delete("/{post_id}")
+def deletar_post(post_id: int):
+    sucesso = PostService.deletar_post(post_id)
+    if not sucesso:
+        raise HTTPException(status_code=404, detail="Post não encontrado")
+    return {"mensagem": "Post deletado com sucesso"}
