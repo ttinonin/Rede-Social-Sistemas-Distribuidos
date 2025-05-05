5c788572ec24d69b57395724500791d0ca65c26d
ttinonin <tinonin935@gmail.com>
2025-05-05 12:58:56 -0300

    Setting up CORS

--- server/main.py
+++ server/main.py
@@ -2,8 +2,21 @@ from fastapi import FastAPI
 from controller import UserController, PostController, ChatController
 from models import Base
 from database import engine
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",  # Vite (React)
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Inclui o controller
 app.include_router(UserController.router)
