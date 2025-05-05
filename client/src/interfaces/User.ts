bb4fef910e6c3bd5b39ff7683e7aa2c5919937f7
ttinonin <tinonin935@gmail.com>
2025-05-05 13:03:12 -0300

    Create user form frontend

--- client/src/interfaces/User.ts
+++ client/src/interfaces/User.ts
@@ -2,5 +2,7 @@ export interface User {
     id: string;
     username: string;
     email: string;
-    password?: string;
+    nome_completo?: string;
+    bio?: string;
+    foto_perfil_url?: string;
 }
--- client/src/services/userService.ts
+++ client/src/services/userService.ts
@@ -2,6 +2,6 @@ import api from "./api";
 import { User } from "../interfaces/User";
 
 export const signUp = async (username: string, email: string, password: string): Promise<User> => {
-    const res = await api.post<User>("/users/", { username, email, password });
+    const res = await api.post<User>("/users/", { username, email, "senha": password });
     return res.data;
 };
